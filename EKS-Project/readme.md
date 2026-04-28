1bf02e81709acaeb47f4605dfd59ef969de84e68
harathi-mutyam <ehmutyam@gmail.com>
2026-04-29 00:39:35 +0200

    Updated EKS IAM OIDC setup and removed duplicate resources

--- EKS-Project/readme.md
+++ EKS-Project/readme.md
@@ -247,4 +247,3 @@ terraform destroy
 ```
 
 
-
--- EKS-Project/terraform/modules/eks.tf
+++ EKS-Project/terraform/modules/eks.tf
@@ -24,11 +24,11 @@ resource "aws_eks_cluster" "eks" {
 }
 
 
-resource "aws_iam_openid_connect_provider" "eks-oidc" {
+/*resource "aws_iam_openid_connect_provider" "eks-oidc" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.eks-certificate.certificates[0].sha1_fingerprint]
   url             = data.tls_certificate.eks-certificate.url
-}
+}*/
 
 
 resource "aws_eks_addon" "eks-addons" {
